497aaa64f9438fc1bfcd64a76f00f70fe170d9a8
TheGassyNinja <compujohn96@gmail.com>
2022-06-28 21:47:30 -0500

    download .sh

---
+++ arch-install.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+# install pkgs
+sudo pacman -Syu && pacman -S bind-tools cmake git nfs-utils alacritty vivid picom nitrogen tmux exa firefox mc polybar rofi zsh figlet lolcat mpg123 neofetch vlc nerd-fonts-source-code-pro inxi-git expac deluge-gtk copyq gvim 
+
+
+# AUR pkgs
+# install yay
+yay inxi
+
+
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
